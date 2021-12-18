2e0ae3da8cad002f4ccfa64bb43314897eef7749
Satyam Kumar <satyam4540@gmail.com>
2021-12-18 11:58:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 
 // Import models
 const Post = require('./src/models/post');
-
 // Define application
 const app = express()
 
@@ -25,12 +24,13 @@ app.post('/posts', function(req, res) {
   const title = req.body.title
   const author = req.body.author
   const content = req.body.content
-
+  console.log(req.body);
   // Assign values to Post model
   var post = new Post();
-  post.title = title
-  post.author = author
-  post.content = content
+  post.title = title;
+  post.author = author;
+  post.content = content;
+  console.log(post);
 
   // Save the post
   post.save(function(error, savedPost) {
@@ -62,6 +62,42 @@ app.get('/posts', function(req, res) {
 // 2. Create API to update a Post
 // 3. Create API to delete a Post
 
-app.listen(3001, function() {
-  console.log('Server is running at port 3001....')
+//Created Api to get details of a post using its id as an query parameter
+app.get('/post',function(req,res){
+  // console.log(req);
+  // res.status(200).send({text:"Hello"});
+  Post.find({id:req.params.id},function(error,post){
+      if(error){
+        res.status(422).send({error: 'unable to find post'});
+      }
+      else{
+        res.status(200).send(post);
+      }
+  });
+});
+//Created Api to  update a Post using its id as an parameter
+app.post('/updatePost',function(req,res){
+  Post.findOneAndUpdate({id:req.params.id},{author:req.body.author, title : req.body.title, content : req.body.content},function(error,newpost){
+    if(error){
+      console.log("Error in Updating");
+    }
+    else{
+      res.status(200).send(newpost);
+    }
+  })
+});
+//Created Api to delete a Post using its id as an parameter
+app.delete('/deletePost',function(req,res){
+  Post.findOneAndDelete({id:req.params.id},function(error,deletedPost){
+    if(error){
+      res.send(422).send("Error in Deleting post");
+    }
+    else{
+      res.status(200).send(deletedPost);
+    }
+
+  });
 })
+app.listen(3000, function() {
+  console.log('Server is running at port 3000....')
+});
